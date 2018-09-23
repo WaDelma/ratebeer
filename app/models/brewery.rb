c39f98a7f175b989b43f50c6f004fb4b055ba79d
WaDelma <delma@del.ma>
2018-09-23 16:08:59 +0300

    w3 upto e8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,4 +2,10 @@ class Brewery < ApplicationRecord
   include RatingAverage
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+  validates :name, presence: true
+  validate do || if year > Time.now.year then 
+      errors.add(:year, "can't be in the future")
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     include RatingAverage
     validates :username, uniqueness: true,
-                         length: { minimum: 3 }
+                         length: { in: 3..30 }
     has_many :ratings
+    has_many :beers, through: :ratings
 end
