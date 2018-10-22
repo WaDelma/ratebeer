503cda26b3f32804bbbc5ab90bda44081f21a07f
Delma <delma@del.ma>
2018-10-22 12:56:25 +0300

    Fix beermapping API brokeness

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = BeermappingApi.get_place params[:id]
-    return if !@place.url
+    return if @place.nil? || !@place.url
 
     @place.url = URI.decode_www_form(@place.url)[0][0]
     return if URI.parse(@place.url).scheme
@@ -14,7 +14,7 @@ class PlacesController < ApplicationController
 
   def search
     @places = BeermappingApi.places_in(params[:city])
-    if @places.empty?
+    if @places.nil? || @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
       render :index
--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -7,7 +7,7 @@ class BeermappingApi
   end
 
   def self.place(place)
-    return [] if place.empty?
+    return nil if place.empty?
 
     Rails.cache.fetch(place, expires_in: 1.week) { get_place(place) }
   end
@@ -15,11 +15,14 @@ class BeermappingApi
   def self.get_places_in(city)
     raise "BEERMAPPING_APIKEY env variable not defined" if ENV['BEERMAPPING_APIKEY'].nil?
 
-    api_key = ENV['BEERMAPPING_APIKEY']
-    url = "http://beermapping.com/webservice/loccity/#{api_key}/"
+    url = "http://beermapping.com/webservice/loccity/#{ENV['BEERMAPPING_APIKEY']}/"
 
-    response = HTTParty.get "#{url}#{ERB::Util.url_encode city}"
-    places = response.parsed_response["bmp_locations"]["location"]
+    begin
+      places = do_api_request("#{url}#{ERB::Util.url_encode city}")
+    rescue StandardError
+      return []
+    end
+
     return [] if places.is_a?(Hash) && places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
@@ -29,13 +32,27 @@ class BeermappingApi
   def self.get_place(place)
     raise "BEERMAPPING_APIKEY env variable not defined" if ENV['BEERMAPPING_APIKEY'].nil?
 
-    api_key = ENV['BEERMAPPING_APIKEY']
-    url = "http://beermapping.com/webservice/locquery/#{api_key}/"
+    url = "http://beermapping.com/webservice/locquery/#{ENV['BEERMAPPING_APIKEY']}/"
 
-    response = HTTParty.get "#{url}#{place}"
-    place = response.parsed_response["bmp_locations"]["location"]
+    begin
+      place = do_api_request("#{url}#{place}")
+    rescue StandardError
+      return nil
+    end
+
     return nil if place['status'].nil?
 
     Place.new place
+  end
+
+  def self.do_api_request(url)
+    response = HTTParty.get url, format: :plain
+    response = cleanup_api_stupidness(response)
+    response = Hash.from_xml(response)
+    response["bmp_locations"]["location"]
+  end
+
+  def self.cleanup_api_stupidness(response)
+    response.gsub(/&(?!#?[a-z0-9]+;)/, '&amp;')
   end
 end
