4021b6b9087b056a7926c051d0ca2f6ce21e66e5
Delma <delma@del.ma>
2018-10-22 11:26:35 +0300

    Fixing stuff

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,6 +1,50 @@
 class StylesController < ApplicationController
+  before_action :set_style, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :ensure_admin_priviledges, only: [:destroy]
   def index
     @styles = Style.all
+  end
+
+  def show
+  end
+
+  def new
+    @style = Style.new
+  end
+
+  def edit
+  end
+
+  def create
+    @style = Style.new(style_params)
+    if @style.save
+      redirect_to @style, notice: 'Style was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @style.update(style_params)
+      redirect_to @style, notice: 'Style was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @style.destroy
+    redirect_to styles_url, notice: 'Style was successfully destroyed.'
+  end
+
+  private
+
+  def set_style
+    @style = Style.find(params[:id])
+  end
+
+  def style_params
+    params.require(:style).permit(:description, :name)
   end
 end
